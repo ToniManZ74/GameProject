ea40c6330441511d93ae662d5fac306e928ca478
axelopez6 <axel.lc66@gmail.com>
2024-06-20 12:15:10 +0200

    Sistema de combate pulido

--- Assets/Scripts/Hit_Damage.cs
+++ Assets/Scripts/Hit_Damage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,32 @@ using UnityEngine;
 public class Hit_Damage : MonoBehaviour
 {
     public int damage;
+    public new string tag;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemigo")) //Si alcanza al enemigo, se coge el valor de vida que tiene el enemigo y se le resta la cantidad de daño correspondiente
+        
+        if (tag == "Enemy")
         {
-            collision.GetComponent<VidasEnemigo>().vida -= damage;
+            if (collision.CompareTag("Enemy")) //Si alcanza al enemigo, se coge el valor de vida que tiene el enemigo y se le resta la cantidad de daño correspondiente
+            {
+                collision.gameObject.GetComponentInParent<VidasEnemigo>().vida -= damage;
+            }
+
         }
+        
+
+        if (tag == "Jugador")
+        {
+
+            if (collision.CompareTag("Jugador")) //Si el enemigo alcanza al jugador, se llamará a la funcion LoseLife que tiene el jugador para que pierda la cantidad de daño deseada
+            {
+                collision.gameObject.GetComponentInParent<VidasPersonaje>().LoseLife(damage);
+            }
+
+        }
+
+
     }
 
 }
--- Assets/Scripts/VidasEnemigo.cs
+++ Assets/Scripts/VidasEnemigo.cs
@@ -18,4 +18,5 @@ public class VidasEnemigo : MonoBehaviour
             Destroy(gameObject); 
         }
     }
+
 }
